6bc7b1db387d405895cea1dc89087894e1aa084e
spread-satou <66296717+spread-satou@users.noreply.github.com>
2022-07-05 19:54:03 +0900

    メインメニュー

--- CloverusSys/FrmMainMenu.Designer.cs
+++ CloverusSys/FrmMainMenu.Designer.cs
@@ -31,11 +31,21 @@ namespace CloverusSys
         {
             this.BtnCustomers = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnCustomers
             // 
-            this.BtnCustomers.Location = new System.Drawing.Point(49, 39);
+            this.BtnCustomers.Location = new System.Drawing.Point(418, 54);
             this.BtnCustomers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.BtnCustomers.Name = "BtnCustomers";
             this.BtnCustomers.Size = new System.Drawing.Size(87, 34);
@@ -46,7 +56,7 @@ namespace CloverusSys
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(157, 134);
+            this.button1.Location = new System.Drawing.Point(453, 155);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 32);
             this.button1.TabIndex = 1;
@@ -54,17 +64,123 @@ namespace CloverusSys
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(179, 591);
+            this.panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(12, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 37);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "マスター作成";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(12, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 37);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "日次業務";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(12, 329);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 37);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "請求業務";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(12, 372);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 37);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "各種帳票";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(12, 415);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(153, 37);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "その他帳票";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button8.Location = new System.Drawing.Point(29, 59);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(136, 37);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "顧客マスター";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button10.Location = new System.Drawing.Point(29, 152);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(136, 37);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "売上作成";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(29, 195);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 37);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "売上予定生成";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
             // FrmMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(749, 274);
+            this.ClientSize = new System.Drawing.Size(1127, 591);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.BtnCustomers);
             this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmMainMenu";
             this.Text = "メインメニュー";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,6 +189,15 @@ namespace CloverusSys
 
         private System.Windows.Forms.Button BtnCustomers;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
