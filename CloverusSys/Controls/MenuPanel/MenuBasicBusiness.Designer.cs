34686145b23186faf9c3eb4163f0b46b60fefcb6
spread-satou <66296717+spread-satou@users.noreply.github.com>
2022-08-19 15:21:33 +0900

    メニューデザイン修正

--- CloverusSys/Controls/MenuPanel/MenuBasicBusiness.Designer.cs
+++ CloverusSys/Controls/MenuPanel/MenuBasicBusiness.Designer.cs
@@ -46,6 +46,11 @@
             this.button10 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.TxtKeyword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DgvCustomer)).BeginInit();
             this.SuspendLayout();
@@ -92,7 +97,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button4.Location = new System.Drawing.Point(424, 306);
+            this.button4.Location = new System.Drawing.Point(424, 345);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(367, 69);
             this.button4.TabIndex = 6;
@@ -102,7 +107,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button5.Location = new System.Drawing.Point(24, 392);
+            this.button5.Location = new System.Drawing.Point(24, 472);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(767, 69);
             this.button5.TabIndex = 7;
@@ -224,11 +229,66 @@
             this.TxtKeyword.Size = new System.Drawing.Size(679, 31);
             this.TxtKeyword.TabIndex = 0;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(585, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 41);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "↓";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(185, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 41);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "↓";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(585, 422);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 41);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "↓";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(185, 422);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 41);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "↓";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(185, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 41);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "↓";
+            // 
             // MenuBasicBusiness
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.TxtKeyword);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button10);
@@ -273,5 +333,10 @@
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox TxtKeyword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- CloverusSys/Controls/MenuPanel/MenuBillingBusiness.Designer.cs
+++ CloverusSys/Controls/MenuPanel/MenuBillingBusiness.Designer.cs
@@ -50,6 +50,10 @@
             this.button15 = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
             this.button17 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button3
@@ -84,18 +88,19 @@
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.button4.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button4.Location = new System.Drawing.Point(24, 118);
+            this.button4.Location = new System.Drawing.Point(24, 169);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(367, 69);
             this.button4.TabIndex = 6;
             this.button4.Text = "予定修正・確認";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(424, 202);
+            this.label1.Location = new System.Drawing.Point(424, 510);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(180, 24);
             this.label1.TabIndex = 7;
@@ -103,28 +108,30 @@
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.Red;
             this.button5.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button5.Location = new System.Drawing.Point(424, 229);
+            this.button5.Location = new System.Drawing.Point(424, 538);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(367, 69);
             this.button5.TabIndex = 8;
             this.button5.Text = "請求先設定キャンセル";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.Red;
             this.button6.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button6.Location = new System.Drawing.Point(424, 342);
+            this.button6.Location = new System.Drawing.Point(424, 693);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(367, 69);
             this.button6.TabIndex = 10;
             this.button6.Text = "請求締日更新取消（個別）";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(424, 315);
+            this.label2.Location = new System.Drawing.Point(424, 666);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(239, 24);
             this.label2.TabIndex = 9;
@@ -132,18 +139,19 @@
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.Color.LightGreen;
             this.button7.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button7.Location = new System.Drawing.Point(424, 458);
+            this.button7.Location = new System.Drawing.Point(824, 42);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(367, 69);
             this.button7.TabIndex = 12;
             this.button7.Text = "入金処理（締請求）";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(424, 431);
+            this.label3.Location = new System.Drawing.Point(824, 14);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 24);
             this.label3.TabIndex = 11;
@@ -152,7 +160,7 @@
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button8.Location = new System.Drawing.Point(424, 533);
+            this.button8.Location = new System.Drawing.Point(824, 118);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(367, 69);
             this.button8.TabIndex = 13;
@@ -162,7 +170,7 @@
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button9.Location = new System.Drawing.Point(428, 720);
+            this.button9.Location = new System.Drawing.Point(824, 371);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(367, 69);
             this.button9.TabIndex = 16;
@@ -172,7 +180,7 @@
             // button10
             // 
             this.button10.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button10.Location = new System.Drawing.Point(428, 645);
+            this.button10.Location = new System.Drawing.Point(824, 296);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(367, 69);
             this.button10.TabIndex = 15;
@@ -182,7 +190,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(428, 618);
+            this.label4.Location = new System.Drawing.Point(824, 268);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 24);
             this.label4.TabIndex = 14;
@@ -191,7 +199,7 @@
             // button11
             // 
             this.button11.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button11.Location = new System.Drawing.Point(428, 909);
+            this.button11.Location = new System.Drawing.Point(824, 711);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(367, 69);
             this.button11.TabIndex = 19;
@@ -201,7 +209,7 @@
             // button12
             // 
             this.button12.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button12.Location = new System.Drawing.Point(428, 834);
+            this.button12.Location = new System.Drawing.Point(824, 636);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(367, 69);
             this.button12.TabIndex = 18;
@@ -211,7 +219,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(428, 807);
+            this.label5.Location = new System.Drawing.Point(824, 609);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(196, 24);
             this.label5.TabIndex = 17;
@@ -219,28 +227,30 @@
             // 
             // button13
             // 
+            this.button13.BackColor = System.Drawing.Color.LightGreen;
             this.button13.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button13.Location = new System.Drawing.Point(24, 909);
+            this.button13.Location = new System.Drawing.Point(24, 693);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(367, 69);
             this.button13.TabIndex = 20;
             this.button13.Text = "請求締日更新";
-            this.button13.UseVisualStyleBackColor = true;
+            this.button13.UseVisualStyleBackColor = false;
             // 
             // button14
             // 
+            this.button14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.button14.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button14.Location = new System.Drawing.Point(24, 229);
+            this.button14.Location = new System.Drawing.Point(24, 296);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(367, 69);
             this.button14.TabIndex = 21;
             this.button14.Text = "請求書発行（口振用）";
-            this.button14.UseVisualStyleBackColor = true;
+            this.button14.UseVisualStyleBackColor = false;
             // 
             // button15
             // 
             this.button15.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button15.Location = new System.Drawing.Point(24, 305);
+            this.button15.Location = new System.Drawing.Point(24, 371);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(367, 69);
             this.button15.TabIndex = 22;
@@ -250,7 +260,7 @@
             // button16
             // 
             this.button16.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button16.Location = new System.Drawing.Point(24, 381);
+            this.button16.Location = new System.Drawing.Point(24, 446);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(367, 69);
             this.button16.TabIndex = 23;
@@ -260,18 +270,62 @@
             // button17
             // 
             this.button17.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button17.Location = new System.Drawing.Point(24, 720);
+            this.button17.Location = new System.Drawing.Point(24, 538);
             this.button17.Name = "button17";
             this.button17.Size = new System.Drawing.Size(367, 69);
             this.button17.TabIndex = 24;
             this.button17.Text = "顧客別請求一覧表";
             this.button17.UseVisualStyleBackColor = true;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(185, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 41);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "↓";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(185, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 41);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "↓";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("メイリオ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(185, 616);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 41);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "↓";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label9.Location = new System.Drawing.Point(24, 666);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(236, 24);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "※更新前に請求書をPDF保存！";
+            // 
             // MenuBillingBusiness
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button17);
             this.Controls.Add(this.button16);
             this.Controls.Add(this.button15);
@@ -327,5 +381,9 @@
         private System.Windows.Forms.Button button15;
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
